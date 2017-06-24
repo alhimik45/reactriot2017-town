155f0eee9e34be31fd010441c92b46a94c16fdd2
alhimik45 <alhimik45@gmail.com>
2017-06-24 09:50:10 +0700

    add inactive state to Building

--- src/components/Building.jsx
+++ src/components/Building.jsx
@@ -8,10 +8,11 @@ export default class Building extends Component {
   // noinspection JSUnusedGlobalSymbols,JSUnresolvedVariable
   static propTypes = {
     name: PropTypes.string.isRequired,
+    upgradeAvailable: PropTypes.bool.isRequired,
     level: PropTypes.number,
-    upgrading: PropTypes.number,
+    upgradeProgress: PropTypes.number,
     imgSrc: PropTypes.string,
-    onLevelUp: PropTypes.func
+    onUpgradeClick: PropTypes.func
   }
 
   render () {
@@ -19,24 +20,32 @@ export default class Building extends Component {
     if (this.props.imgSrc) {
       imgBg = `url(${this.props.imgSrc}) no-repeat,`
     }
-    const progress = this.props.upgrading || 0
+    const progress = this.props.upgradeProgress || 0
     const background = css({
       background: `${imgBg} linear-gradient(to right, rgba(0,0,255,0.4) ${progress}%, #ffffff ${progress}%)`,
       backgroundSize: 'contain',
       backgroundPosition: 'center'
     })
+    const fullVisibility = css({
+      opacity: !this.props.upgradeAvailable && (!this.props.level || this.props.level === 0) ? 0.5 : 1
+    })
+
+    const buttonVisibility = css({
+      opacity: !this.props.upgradeAvailable && this.props.level ? 0.5 : 1
+    })
 
     return (
-      <div {...css(boldBorder, background)}>
+      <div {...css(boldBorder, background, fullVisibility)}>
         <div className='text-center'>
           {this.props.name}
         </div>
         <b.Row>
           <b.Col xs={6} className='text-center'>
             <img
+              {...buttonVisibility}
               src='/static/plus.svg'
               width={25}
-              onClick={this.props.onLevelUp}
+              onClick={this.props.onUpgradeClick}
             />
           </b.Col>
           <b.Col xs={6} className='text-center'>
