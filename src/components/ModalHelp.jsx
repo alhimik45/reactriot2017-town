1b5132560db71a5d85e076041b82235e9bae6d75
RightJelkin <romgalkin23@gmail.com>
2017-06-25 20:51:39 +0700

    HOW TO PLAY modal. Rules and controls inside

--- src/components/ModalHelp.jsx
+++ src/components/ModalHelp.jsx
@@ -26,14 +26,27 @@ export default class ModalHelp extends Component {
           bsStyle='info'
           onClick={this.open}
         >
-          <b.Glyphicon glyph='question-sign' />
+          <b.Glyphicon glyph='question-sign' /> HOW TO PLAY
         </b.Button>
         <b.Modal show={this.state.showModal} onHide={this.close}>
           <b.Modal.Header closeButton>
             <b.Modal.Title>Help</b.Modal.Title>
           </b.Modal.Header>
           <b.Modal.Body>
-            <h4>Text for help with components.</h4>
+            <h4>Hello.</h4>
+            <p>You are manager of a small strange and unstable town.</p>
+            <p>Your people can be a miners, lumberjacks, farmers and hunters. Each of them can give you resources. You can see list of resources on the top right corner. In the top of screen you can see the rate of resources extraction</p>
+            <p>It will take some resources to teach idle man to worker. Hover on unit to learn how much.</p>
+            <p>You can build some buildings. Buildings give your some effects. You can read about them by hover on building.</p>
+            <p>Sometimes your people can go criminal for stealing your resources. Criminals can become an idle people back. Your city statistics (population and professions) displayed on the right part of screen</p>
+            <p>You can control taxes from, your people (slider on right bottom side), it will give your more money per tick, but increase anger of population.</p>
+            <p>Anger affects on chance, that worker or idle people can become criminals</p>
+            <p>When anger is very big, you people can start RIOT. Rioters will quickly destroy your resources!</p>
+            <p>You have some mortality level (panel "Info" on left side). Your people will die faster without food (obviously)</p>
+            <p>You can build ARMY for some aggressive actions against neighbors. Just recruit them and press "WAR" button. In the war you can take away some resources from enemies...or you can lose all your soldiers. Remember, that army will spend a money (the bigger army, the more money)</p>
+            <h4>Controls</h4>
+            <p>You can control the speed of game by buttons (left panel under "Info). It has a 0 value by default. (x1 to start the game)</p>
+            <p>You can queue up several units at once. (1, 10 or 100). It controls by buttons on "Trainings" block. (left top side) </p>
           </b.Modal.Body>
           <b.Modal.Footer>
             <b.Button onClick={this.close}>Close</b.Button>
